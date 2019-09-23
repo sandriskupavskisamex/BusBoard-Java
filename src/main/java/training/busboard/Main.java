835e2fca53b4f7d9493f1018fcabfff86c1904a9
skupavsk <skupavsk@c02yh0wwjgh6.uk.aexp.com>
2019-09-23 14:21:46 +0100

    changes2

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -3,6 +3,6 @@ package training.busboard;
 public class Main {
     public static void main(String args[]) {
         // Your code here!
-        System.out.println("Let's do this!");
+        System.out.println("Let's do thisss!");
     }
 }	
